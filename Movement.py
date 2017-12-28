8665230a72469c048290de175090eb3dbc055979
abhayshah1 <net_abhay@yahoo.com>
2017-12-28 03:16:17 +0000

    Changed to code for motors

--- Movement.py
+++ Movement.py
@@ -37,26 +37,37 @@ print "Waiting for connection on RFCOMM channel %d" % port
 client_sock, client_info = server_sock.accept()
 print "Accepted connection from ", client_info
 
+'''
+Directional code is <Motor><F/R><Speed 0-100>
+Example:
+LF50 = Left Motor, Forward, Speed 50
+LR25 = Left Motor, Reverse, Speed 25
+RR25 = Right Motor, Reverse, Speed 25
+RF100 = Right Motor, Forward, Speed 100
+'''
+
 try:
     while True:
-        direction = client_sock.recv(1024)
-        if ( direction == "FORWARD" ):
-            rightMotor.forward(100)
-            leftMotor.forward(100)
-        elif ( direction == "REVERSE" ):
-            rightMotor.reverse(100)
-            leftMotor.reverse(100)
-        elif ( direction == "LEFT" ):
-            rightMotor.forward(100)
-            leftMotor.reverse(100)
-        elif( direction == "RIGHT" ):
-            rightMotor.reverse(100)
-            leftMotor.forward(100)
-        elif len(direction) == 0: break
-        print "received [%s]" % direction
-        sleep(2)
-        rightMotor.stop()
-        leftMotor.stop()
+        data = client_sock.recv(1024)
+        if ( len(data) != 0 ):
+            # some string received
+            print "received [%s]" % data
+            motor = data[0]
+            direction = data[1]
+            speed = data[2:]
+            if ( speed < 0 or speed > 100 ):
+                print "Defaulting speed to 100"
+                speed = 100
+            if ( motor == "R" ):
+                if ( direction == "F" ):
+                    rightMotor.forward(speed)
+                elif ( direction == "R" ):
+                    rightMotor.reverse(speed)
+            if ( motor == "L" ):
+                if ( direction == "F" ):
+                    leftMotor.forward(speed)
+                elif( direction == "R" ):
+                    leftMotor.reverse(speed)
 except IOError:
     pass
 

3697afcbd02085cbfda8ddd9490fde90242d83ed
abhayshah1 <net_abhay@yahoo.com>
2018-07-04 16:24:37 +0000

    Added comments to constants file indicating board numbering scheme

--- MoveConstants.py
+++ MoveConstants.py
@@ -1,8 +1,9 @@
-#constants for GPIO pins
+#constants for GPIO pins. These pin numbers correspond to BCM numbering
 GPIO_FORWARD_PIN = 20
 GPIO_BACKWARD_PIN = 21
 GPIO_MOTOR_PIN = 16
 GPIO_RIGHT_PIN = 19
 
+#Motor configuration from PiMotor library. PiMotor internally uses Board numbering
 LEFT_MOTOR = "MOTOR1"
 RIGHT_MOTOR = "MOTOR2"
